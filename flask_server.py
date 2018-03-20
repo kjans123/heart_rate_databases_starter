44b94ea8f16323e6fcb64454d59829166380127e
kjans123 <kj123@duke.edu>
2018-03-20 16:25:31 -0400

    init commit for server. Includes POST section

---
+++ flask_server.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+import requests
+import datetime
+from main import add_heart_rate, create_user, print_user
+app = Flask(__name__)
+
+@app.route("/new_heart_rate", methods=["POST"])
+def add_new_hr():
+    r = requests.get_json()
+    email = r["user_email"]
+    age = r["user_age"]
+    heart_rate = r["heart_rate"]
+    curr_datetime = datetime.datetime.now()
+    connect("mongodb://vcm-3594.vm.duke.edu:27017/heart_rate_app")
+    add_heart_rate(email, heart_rate, curr_datetime)
+    return_dict = {
+        "user_email": str(email),
+        "user_age": str(age),
+        "heart_rate": str(heart_rate)
+                }
+    return jsonify(return_dict), 200
