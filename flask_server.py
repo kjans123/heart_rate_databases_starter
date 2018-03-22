5da5ce57da461ad710f9526145cdc521a5197ab4
kjans123 <kj123@duke.edu>
2018-03-21 23:00:16 -0400

    debugging try/except v2 datetime

--- flask_server.py
+++ flask_server.py
@@ -63,28 +63,23 @@ def interval_average():
     r = request.get_json()
     email = r["user_email"]
     input_date_time = r["date_time"]
-    try:
-        validate_date_time(input_date_time)
-        time_list = get_all_times(email)
-        for i in range(len(time_list)):
-            if date_time <= time_list[i]:
-                final_date_index = i
-                break
-        heart_rate_list = get_all_rates(email)
-        interval_list = []
-        for i in range(len(heart_rate_list)):
-            if i <= final_date_index:
-                interval_list.append(heart_rate_list[i])
-            else:
-                break
-        interval_average_post = st.mean(interval_list)
-        return_dict = {
-            "user_email": email,
-            "heart_rate_average_since": str(date_time),
-            "heart_rate_average": interval_average_post
-                    }
-        return jsonify(return_dict)
-    except TypeError:
-        print("check date list")
-    #except ValueError:
-        #print("invalid element in date list")
+    validate_date_time(input_date_time)
+    time_list = get_all_times(email)
+    for i in range(len(time_list)):
+        if date_time <= time_list[i]:
+            final_date_index = i
+            break
+    heart_rate_list = get_all_rates(email)
+    interval_list = []
+    for i in range(len(heart_rate_list)):
+        if i <= final_date_index:
+            interval_list.append(heart_rate_list[i])
+        else:
+            break
+    interval_average_post = st.mean(interval_list)
+    return_dict = {
+        "user_email": email,
+        "heart_rate_average_since": str(date_time),
+        "heart_rate_average": interval_average_post
+                  }
+    return jsonify(return_dict)
